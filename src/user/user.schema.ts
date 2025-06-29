63eec7812c4ac44829ade1be0e1401f02b352d18
paula <paula@omniesolutions.com>
2025-06-30 00:57:14 +0530

    transform _id to id

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -14,5 +14,12 @@ export class User {
 }
 
 export const USER_SCHEMA = SchemaFactory.createForClass(User);
+USER_SCHEMA.set('toJSON', {
+  virtuals: true, // this converts _id to id
+  transform: (doc, converted) => {
+    delete converted._id;
+  },
+});
+export default USER_SCHEMA;
 export const USER_NAME = User.name;
 export type USER_DOCUMENT = User & Document;
